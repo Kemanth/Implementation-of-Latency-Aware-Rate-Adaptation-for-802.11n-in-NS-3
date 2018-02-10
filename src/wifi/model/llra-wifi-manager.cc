485343e6d4d67c65e02673b5784607148e3ea842
Kemanth <kemanth.raj@gmail.com>
2018-02-10 15:09:58 +0530

    wifi: added method to initialize the wifi modes

---
+++ src/wifi/model/llra-wifi-manager.cc
@@ -0,0 +1,283 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+
+#include "llra-wifi-manager.h"
+#include "ns3/log.h"
+#include "ns3/uinteger.h"
+
+#define Min(a,b) ((a < b) ? a : b)
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("LlraWifiManager");
+
+/**
+ * \brief hold per-remote-station state for LLRA Wifi manager.
+ *
+ * This struct extends from WifiRemoteStation struct to hold additional
+ * information required by the LLRA Wifi manager
+ */
+struct LlraWifiRemoteStation : public WifiRemoteStation
+{
+  uint32_t m_timer; ///< timer value
+  uint32_t m_success; ///< success count
+  uint32_t m_failed; ///< failed count
+  bool m_recovery; ///< recovery
+  uint32_t m_retry; ///< retry count
+  uint32_t m_timerTimeout; ///< timer timeout
+  uint32_t m_rate; ///< rate
+  uint32_t m_alpha; ///< alpha percentile
+};
+
+NS_OBJECT_ENSURE_REGISTERED (LlraWifiManager);
+
+TypeId
+LlraWifiManager::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::LaraWifiManager")
+    .SetParent<WifiRemoteStationManager> ()
+    .SetGroupName ("Wifi")
+    .AddConstructor<LaraWifiManager> ()
+    .AddAttribute ("Alpha", "The alpha percentile.",
+                   UintegerValue (95),
+                   MakeUintegerAccessor (&LaraWifiManager::m_alpha),
+                   MakeUintegerChecker<uint32_t> ())
+    .AddTraceSource ("Rate",
+                     "Traced value for rate changes (b/s)",
+                     MakeTraceSourceAccessor (&LaraWifiManager::m_currentRate),
+                     "ns3::TracedValueCallback::Uint64")
+  ;
+  return tid;
+}
+
+
+LlraWifiManager::LlraWifiManager ()
+  : m_currentRate (0)
+{
+}
+
+LlraWifiManager::~LlraWifiManager ()
+{
+}
+
+void
+LlraWifiManager::SetupPhy (const Ptr<WifiPhy> phy)
+{
+  NS_LOG_FUNCTION (this << phy);
+  WifiRemoteStationManager::SetupPhy (phy);
+}
+
+uint8_t
+LlraWifiManager::GetChannelWidthForMode (WifiMode mode) const
+{
+  NS_ASSERT (mode.GetModulationClass () != WIFI_MOD_CLASS_HT
+             && mode.GetModulationClass () != WIFI_MOD_CLASS_VHT
+             && mode.GetModulationClass () != WIFI_MOD_CLASS_HE);
+  if (mode.GetModulationClass () == WIFI_MOD_CLASS_DSSS
+      || mode.GetModulationClass () == WIFI_MOD_CLASS_HR_DSSS)
+    {
+      return 22;
+    }
+  else
+    {
+      return 20;
+    }
+}
+
+void
+LlraWifiManager::DoInitialize ()
+{
+  NS_LOG_FUNCTION (this);
+  WifiMode mode;
+  WifiTxVector txVector;
+  uint8_t nss = 1;
+  uint8_t nModes = GetPhy ()->GetNModes ();
+  for (uint8_t i = 0; i < nModes; i++)
+    {
+      mode = GetPhy ()->GetMode (i);
+      txVector.SetChannelWidth (GetChannelWidthForMode (mode));
+      txVector.SetNss (nss);
+      txVector.SetMode (mode);
+      NS_LOG_DEBUG ("Initialize, adding mode = " << mode.GetUniqueName ());
+      AddSnrThreshold (txVector, GetPhy ()->CalculateSnr (txVector, m_ber));
+    }
+  // Add all Ht and Vht MCSes
+  if (HasVhtSupported () == true || HasHtSupported () == true || HasHeSupported () == true)
+    {
+      nModes = GetPhy ()->GetNMcs ();
+      for (uint8_t i = 0; i < nModes; i++)
+        {
+          for (uint16_t j = 20; j <= GetPhy ()->GetChannelWidth (); j *= 2)
+            {
+              txVector.SetChannelWidth (j);
+              mode = GetPhy ()->GetMcs (i);
+              if (mode.GetModulationClass () == WIFI_MOD_CLASS_HT)
+                {
+                  uint16_t guardInterval = GetPhy ()->GetShortGuardInterval () ? 400 : 800;
+                  txVector.SetGuardInterval (guardInterval);
+                  //derive NSS from the MCS index
+                  nss = (mode.GetMcsValue () / 8) + 1;
+                  NS_LOG_DEBUG ("Initialize, adding mode = " << mode.GetUniqueName () <<
+                                " channel width " << (uint16_t) j <<
+                                " nss " << (uint16_t) nss <<
+                                " GI " << guardInterval);
+                  NS_LOG_DEBUG ("In SetupPhy, adding mode = " << mode.GetUniqueName ());
+                  txVector.SetNss (nss);
+                  txVector.SetMode (mode);
+                  AddSnrThreshold (txVector, GetPhy ()->CalculateSnr (txVector, m_ber));
+                }
+              else //VHT or HE
+                {
+                  uint16_t guardInterval;
+                  if (mode.GetModulationClass () == WIFI_MOD_CLASS_VHT)
+                    {
+                      guardInterval = GetPhy ()->GetShortGuardInterval () ? 400 : 800;
+                    }
+                  else
+                    {
+                      guardInterval = GetPhy ()->GetGuardInterval ().GetNanoSeconds ();
+                    }
+                  for (uint8_t i = 1; i <= GetPhy ()->GetMaxSupportedTxSpatialStreams (); i++)
+                    {
+                      NS_LOG_DEBUG ("Initialize, adding mode = " << mode.GetUniqueName () <<
+                                    " channel width " << (uint16_t) j <<
+                                    " nss " << (uint16_t) i <<
+                                    " GI " << guardInterval);
+                      NS_LOG_DEBUG ("In SetupPhy, adding mode = " << mode.GetUniqueName ());
+                      txVector.SetNss (i);
+                      txVector.SetMode (mode);
+                      AddSnrThreshold (txVector, GetPhy ()->CalculateSnr (txVector, m_ber));
+                    }
+                }
+            }
+        }
+    }
+}
+
+double
+LlraWifiManager::GetSnrThreshold (WifiTxVector txVector) const
+{
+  NS_LOG_FUNCTION (this << txVector.GetMode ().GetUniqueName ());
+  for (Thresholds::const_iterator i = m_thresholds.begin (); i != m_thresholds.end (); i++)
+    {
+      NS_LOG_DEBUG ("Checking " << i->second.GetMode ().GetUniqueName () <<
+                    " nss " << (uint16_t) i->second.GetNss () <<
+                    " GI " << i->second.GetGuardInterval () <<
+                    " width " << (uint16_t) i->second.GetChannelWidth ());
+      NS_LOG_DEBUG ("against TxVector " << txVector.GetMode ().GetUniqueName () <<
+                    " nss " << (uint16_t) txVector.GetNss () <<
+                    " GI " << txVector.GetGuardInterval () <<
+                    " width " << (uint16_t) txVector.GetChannelWidth ());
+      if (txVector.GetMode () == i->second.GetMode ()
+          && txVector.GetNss () == i->second.GetNss ()
+          && txVector.GetChannelWidth () == i->second.GetChannelWidth ())
+        {
+          return i->first;
+        }
+    }
+  NS_ASSERT (false);
+  return 0.0;
+}
+
+void
+LlraWifiManager::AddSnrThreshold (WifiTxVector txVector, double snr)
+{
+  NS_LOG_FUNCTION (this << txVector.GetMode ().GetUniqueName () << snr);
+  m_thresholds.push_back (std::make_pair (snr, txVector));
+}
+
+WifiRemoteStation *
+LlraWifiManager::DoCreateStation (void) const
+{
+  NS_LOG_FUNCTION (this);
+  LlraWifiRemoteStation *station = new LlraWifiRemoteStation ();
+  station->m_lastSnrObserved = 0.0;
+  station->m_lastSnrCached = CACHE_INITIAL_VALUE;
+  station->m_lastMode = GetDefaultMode ();
+  station->m_nss = 1;
+  return station;
+}
+
+
+void
+LlraWifiManager::DoReportRxOk (WifiRemoteStation *station,
+                                double rxSnr, WifiMode txMode)
+{
+}
+
+void
+LlraWifiManager::DoReportRtsFailed (WifiRemoteStation *station)
+{
+}
+
+void
+LlraWifiManager::DoReportDataFailed (WifiRemoteStation *station)
+{
+}
+
+void
+LlraWifiManager::DoReportRtsOk (WifiRemoteStation *st,
+                                 double ctsSnr, WifiMode ctsMode, double rtsSnr)
+{
+  NS_LOG_FUNCTION (this << st << ctsSnr << ctsMode.GetUniqueName () << rtsSnr);
+  LlraWifiRemoteStation *station = (LlraWifiRemoteStation *)st;
+  station->m_lastSnrObserved = rtsSnr;
+}
+
+void
+LlraWifiManager::DoReportDataOk (WifiRemoteStation *st,
+                                  double ackSnr, WifiMode ackMode, double dataSnr)
+{
+  NS_LOG_FUNCTION (this << st << ackSnr << ackMode.GetUniqueName () << dataSnr);
+  LlraWifiRemoteStation *station = (LlraWifiRemoteStation *)st;
+  if (dataSnr == 0)
+    {
+      NS_LOG_WARN ("DataSnr reported to be zero; not saving this report.");
+      return;
+    }
+  station->m_lastSnrObserved = dataSnr;
+}
+
+void
+LlraWifiManager::DoReportAmpduTxStatus (WifiRemoteStation *st, uint8_t nSuccessfulMpdus, uint8_t nFailedMpdus, double rxSnr, double dataSnr)
+{
+  NS_LOG_FUNCTION (this << st << (uint16_t)nSuccessfulMpdus << (uint16_t)nFailedMpdus << rxSnr << dataSnr);
+  LlraWifiRemoteStation *station = (LlraWifiRemoteStation *)st;
+  if (dataSnr == 0)
+    {
+      NS_LOG_WARN ("DataSnr reported to be zero; not saving this report.");
+      return;
+    }
+  station->m_lastSnrObserved = dataSnr;
+}
+
+
+void
+LlraWifiManager::DoReportFinalRtsFailed (WifiRemoteStation *station)
+{
+}
+
+void
+LlraWifiManager::DoReportFinalDataFailed (WifiRemoteStation *station)
+{
+}
+
+WifiTxVector
+LlraWifiManager::DoGetDataTxVector (WifiRemoteStation *st)
+{
+}
+
+WifiTxVector
+LlraWifiManager::DoGetRtsTxVector (WifiRemoteStation *st)
+{
+}
+
+bool
+LlraWifiManager::IsLowLatency (void) const
+{
+  return true;
+}
+
+} //namespace ns3
+
+
---
+++ src/wifi/model/llra-wifi-manager.h
@@ -0,0 +1,83 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+
+#ifndef LLRA_WIFI_MANAGER_H
+#define LLRA_WIFI_MANAGER_H
+
+#include "ns3/traced-value.h"
+#include "wifi-remote-station-manager.h"
+
+namespace ns3 {
+
+class LlraWifiManager : public WifiRemoteStationManager
+{
+public:
+  /**
+   * \brief Get the type ID.
+   * \return the object TypeId
+   */
+  static TypeId GetTypeId (void);
+  LlraWifiManager ();
+  virtual ~LlraWifiManager ();
+  
+  void SetupPhy (const Ptr<WifiPhy> phy);  
+
+private:
+  //overriden from base class
+  WifiRemoteStation * DoCreateStation (void) const;
+  void DoReportRxOk (WifiRemoteStation *station,
+                     double rxSnr, WifiMode txMode);
+  void DoReportRtsFailed (WifiRemoteStation *station);
+  void DoReportDataFailed (WifiRemoteStation *station);
+  void DoReportRtsOk (WifiRemoteStation *station,
+                      double ctsSnr, WifiMode ctsMode, double rtsSnr);
+  void DoReportDataOk (WifiRemoteStation *station,
+                       double ackSnr, WifiMode ackMode, double dataSnr);
+  void DoReportFinalRtsFailed (WifiRemoteStation *station);
+  void DoReportFinalDataFailed (WifiRemoteStation *station);
+  WifiTxVector DoGetDataTxVector (WifiRemoteStation *station);
+  WifiTxVector DoGetRtsTxVector (WifiRemoteStation *station);
+  bool IsLowLatency (void) const;
+
+  uint32_t m_timerThreshold; ///< timer threshold
+  uint32_t m_alpha; ///< alpha percentile
+    
+  /**
+   * Return the minimum SNR needed to successfully transmit
+   * data with this WifiTxVector at the specified BER.
+   *
+   * \param txVector WifiTxVector (containing valid mode, width, and nss)
+   *
+   * \return the minimum SNR for the given WifiTxVector
+   */
+  double GetSnrThreshold (WifiTxVector txVector) const;
+  /**
+   * Adds a pair of WifiTxVector and the minimum SNR for that given vector
+   * to the list.
+   *
+   * \param txVector the WifiTxVector storing mode, channel width, and nss
+   * \param snr the minimum SNR for the given txVector
+   */
+  void AddSnrThreshold (WifiTxVector txVector, double snr);
+
+  /**
+   * Convenience function for selecting a channel width for legacy mode
+   * \param mode non-(V)HT WifiMode
+   * \return the channel width (MHz) for the selected mode
+   */
+  uint8_t GetChannelWidthForMode (WifiMode mode) const;
+
+  /**
+   * A vector of <snr, WifiTxVector> pair holding the minimum SNR for the
+   * WifiTxVector
+   */
+  typedef std::vector<std::pair<double, WifiTxVector> > Thresholds;
+
+  double m_ber;             //!< The maximum Bit Error Rate acceptable at any transmission mode
+  Thresholds m_thresholds;  //!< List of WifiTxVector and the minimum SNR pair
+  TracedValue<uint64_t> m_currentRate; //!< Trace rate changes
+};
+
+} //namespace ns3
+
+#endif /* LLRA_WIFI_MANAGER_H */
